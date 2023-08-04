7fea1bf66cc4b7c9efe3be5460ee3e76f1505225
Nathan Amaral <109834646+nathan5d@users.noreply.github.com>
2023-08-04 11:26:23 -0300

    atualizar pdf

--- assets/js/app.js
+++ assets/js/app.js
@@ -1012,14 +1012,20 @@ $(document).ready(function () {
         // Adiciona a tabela de despesas ao documento PDF
         addTableToPDF(doc, despesasTable, 'Despesas', startY);
 
-        doc.setFont(undefined, 'bold');
-        doc.setFontSize(12);
+        doc.setFont(undefined, 'normal');
+        doc.setFontSize(11);
         doc.setTextColor(0, 0, 0);
         // Adicionar título da tabela alinhado à direita
        
        
+
         doc.text('Despesas Pagas R$ ' + somaDespesasPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 15, { align: 'right' });
         doc.text('Despesas Não Pagas R$ ' + somaDespesasNaoPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 20, { align: 'right' });
+
+        doc.setFont(undefined, 'bold');
+        doc.setFontSize(11);
+        doc.setTextColor(0, 0, 0);
+        doc.text('Balanço R$ ' + balanco.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 35, { align: 'right' });
 
 
         // Adicionar a imagem centralizada no cabeçalho da primeira página
