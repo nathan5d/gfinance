b58fcf5697d0b7f9521e01148a5091ec465afb5c
Nathan Amaral <109834646+nathan5d@users.noreply.github.com>
2023-08-04 14:33:11 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1022,7 +1022,7 @@ $(document).ready(function () {
         // Adicionar título da tabela alinhado à direita
         let textWidth = 'Pagas ' + somaDespesasPagas.text();
 
-        doc.text( 'Pagas ', doc.internal.pageSize.getWidth() - doc.getTextWidth(textWidth), doc.lastAutoTable.finalY + 10, { align: 'right' });
+        doc.text( 'Pagas ', doc.internal.pageSize.getWidth() - doc.getTextWidth(textWidth.toString()), doc.lastAutoTable.finalY + 10, { align: 'right' });
         doc.setFont(undefined, 'bold');
         doc.text('R$ ' + somaDespesasPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.lastAutoTable.finalY + 10, { align: 'right' });
 
@@ -1031,7 +1031,7 @@ $(document).ready(function () {
         doc.setFont(undefined, 'normal');
 
         textWidth = 'Não Pagas ' + somaDespesasNaoPagas.text()
-        doc.text('Não Pagas ', doc.internal.pageSize.getWidth() - doc.getTextWidth(textWidth), doc.lastAutoTable.finalY + 15, { align: 'right' });
+        doc.text('Não Pagas ', doc.internal.pageSize.getWidth() - doc.getTextWidth(textWidth.toString()), doc.lastAutoTable.finalY + 15, { align: 'right' });
         
         doc.setFont(undefined, 'bold');
         doc.text('R$ ' + somaDespesasNaoPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.lastAutoTable.finalY + 15, { align: 'right' });
