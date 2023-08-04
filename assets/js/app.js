a27109c8524f7f6b6449d2fd9e46cf922384b90f
Nathan Amaral <109834646+nathan5d@users.noreply.github.com>
2023-08-04 11:01:38 -0300

    atualizar pdf

--- assets/js/app.js
+++ assets/js/app.js
@@ -1016,12 +1016,10 @@ $(document).ready(function () {
         doc.setFontSize(12);
         doc.setTextColor(0, 0, 0);
         // Adicionar título da tabela alinhado à direita
-        doc.text('Balanço', doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 10, { align: 'right' });
-
-        doc.text('R$ ' + balanco.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 20, { align: 'right' });
-
-        doc.text('Despesas Pagas R$ ' + somaDespesasPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 30, { align: 'right' });
-        doc.text('Despesas Não Pagas R$ ' + somaDespesasNaoPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 40, { align: 'right' });
+       
+       
+        doc.text('Despesas Pagas R$ ' + somaDespesasPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 15, { align: 'right' });
+        doc.text('Despesas Não Pagas R$ ' + somaDespesasNaoPagas.text(), doc.internal.pageSize.getWidth() - 15, doc.autoTable.previous.finalY + 20, { align: 'right' });
 
 
         // Adicionar a imagem centralizada no cabeçalho da primeira página
